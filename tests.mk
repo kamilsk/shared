9fad127140850b2c0e3253abb9b08d41d9627979
Kamil Samigullin <kamil@samigullin.info>
2016-12-04 22:19:11 +0300

    rename env var

--- tests.mk
+++ tests.mk
@@ -5,7 +5,7 @@
 #
 
 GO_TEST_COVERAGE_MODE?="count"
-GO_TEST_COVERAGE_FILE_NAME?="coverage.out"
+GO_TEST_COVERAGE_FILENAME?="coverage.out"
 
 .PHONY: test
 test:
@@ -21,8 +21,8 @@ test-with-coverage-formatted:
 
 .PHONY: test-with-coverage-profile
 test-with-coverage-profile:
-	echo "mode: ${GO_TEST_COVERAGE_MODE}" > ${GO_TEST_COVERAGE_FILE_NAME}
+	echo "mode: ${GO_TEST_COVERAGE_MODE}" > ${GO_TEST_COVERAGE_FILENAME}
 	for package in $$(go list ./...); do \
 		go test -covermode ${GO_TEST_COVERAGE_MODE} -coverprofile "coverage_$${package##*/}.out" -race "$${package}"; \
-		sed '1d' "coverage_$${package##*/}.out" >> ${GO_TEST_COVERAGE_FILE_NAME}; \
+		sed '1d' "coverage_$${package##*/}.out" >> ${GO_TEST_COVERAGE_FILENAME}; \
 	done
