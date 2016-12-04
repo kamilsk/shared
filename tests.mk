7c41fb129da610ac081fa074e62c644ba1b37239
Kamil Samigullin <kamil@samigullin.info>
2016-12-04 22:18:18 +0300

    fix issue #3

--- tests.mk
+++ tests.mk
@@ -3,3 +3,26 @@
 #
 # Version: 1.0
 #
+
+GO_TEST_COVERAGE_MODE?="count"
+GO_TEST_COVERAGE_FILE_NAME?="coverage.out"
+
+.PHONY: test
+test:
+	go test -race -v ./...
+
+.PHONY: test-with-coverage
+test-with-coverage:
+	go test -cover -race ./...
+
+.PHONY: test-with-coverage-formatted
+test-with-coverage-formatted:
+	go test -cover -race ./... | column -t | sort -r
+
+.PHONY: test-with-coverage-profile
+test-with-coverage-profile:
+	echo "mode: ${GO_TEST_COVERAGE_MODE}" > ${GO_TEST_COVERAGE_FILE_NAME}
+	for package in $$(go list ./...); do \
+		go test -covermode ${GO_TEST_COVERAGE_MODE} -coverprofile "coverage_$${package##*/}.out" -race "$${package}"; \
+		sed '1d' "coverage_$${package##*/}.out" >> ${GO_TEST_COVERAGE_FILE_NAME}; \
+	done
