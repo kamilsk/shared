aa34ef420e6e9ccbc76c0c044c58db1a2b4f2b6e
Kamil Samigullin <kamil@samigullin.info>
2016-12-25 23:09:28 +0300

    update issue #38

--- docker/tooling.mk
+++ docker/tooling.mk
@@ -4,5 +4,5 @@ docker-tool-gometalinter:
 	docker run --rm \
 	           -v "${GOPATH}/src/${GO_PACKAGE}":"/go/src/${GO_PACKAGE}" \
 	           -w "/go/src/${GO_PACKAGE}" \
-	           kamilsk/go-tools:alpine \
+	           kamilsk/go-tools:latest \
 	           gometalinter.v1 --vendor --deadline=$(DEADLINE) ./...
