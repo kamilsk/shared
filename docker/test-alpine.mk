b43aa7c14c4428d8ad8a62af55c32d096bc2f6eb
Kamil Samigullin <kamil@samigullin.info>
2016-12-26 15:52:34 +0300

    fix issue #42

--- docker/test-alpine.mk
+++ docker/test-alpine.mk
@@ -4,8 +4,7 @@ docker-test-1.5-alpine:
 	           -v "${GOPATH}/src/${GO_PACKAGE}":"/go/src/${GO_PACKAGE}" \
 	           -w "/go/src/${GO_PACKAGE}" \
 	           golang:1.5-alpine \
-	           /bin/sh -c "go list ./... | grep -v /vendor/ | xargs -n1 go get -d -t "$1" && \
-	                       go list ./... | grep -v /vendor/ | xargs -n1 go test -v "$1""
+	           /bin/sh -c "go list ./... | grep -v /vendor/ | xargs -n1 go test -v "$1""
 
 .PHONY: docker-test-1.6-alpine
 docker-test-1.6-alpine:
@@ -13,8 +12,7 @@ docker-test-1.6-alpine:
 	           -v "${GOPATH}/src/${GO_PACKAGE}":"/go/src/${GO_PACKAGE}" \
 	           -w "/go/src/${GO_PACKAGE}" \
 	           golang:1.6-alpine \
-	           /bin/sh -c "go list ./... | grep -v /vendor/ | xargs -n1 go get -d -t "$1" && \
-	                       go list ./... | grep -v /vendor/ | xargs -n1 go test -v "$1""
+	           /bin/sh -c "go list ./... | grep -v /vendor/ | xargs -n1 go test -v "$1""
 
 .PHONY: docker-test-1.7-alpine
 docker-test-1.7-alpine:
@@ -22,8 +20,7 @@ docker-test-1.7-alpine:
 	           -v "${GOPATH}/src/${GO_PACKAGE}":"/go/src/${GO_PACKAGE}" \
 	           -w "/go/src/${GO_PACKAGE}" \
 	           golang:1.7-alpine \
-	           /bin/sh -c "go list ./... | grep -v /vendor/ | xargs -n1 go get -d -t "$1" && \
-	                       go list ./... | grep -v /vendor/ | xargs -n1 go test -v "$1""
+	           /bin/sh -c "go list ./... | grep -v /vendor/ | xargs -n1 go test -v "$1""
 
 .PHONY: docker-test-alpine
 docker-test-alpine:
@@ -31,5 +28,4 @@ docker-test-alpine:
 	           -v "${GOPATH}/src/${GO_PACKAGE}":"/go/src/${GO_PACKAGE}" \
 	           -w "/go/src/${GO_PACKAGE}" \
 	           golang:alpine \
-	           /bin/sh -c "go list ./... | grep -v /vendor/ | xargs -n1 go get -d -t "$1" && \
-	                       go list ./... | grep -v /vendor/ | xargs -n1 go test -v "$1""
+	           /bin/sh -c "go list ./... | grep -v /vendor/ | xargs -n1 go test -v "$1""
