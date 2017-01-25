0f6a7e96b2d219b5b18a0d6463203250dbf49371
Kamil Samigullin <kamil@samigullin.info>
2017-01-25 15:35:09 +0300

    fix issue #78

--- docker/clean.mk
+++ docker/clean.mk
@@ -3,12 +3,22 @@ PRUNE_AVAILABLE := $(shell echo "1.13.0\n$(DOCKER_VERSION)" | sort -ct. -k1,1n -
 PRUNE ?=
 
 .PHONY: docker-clean
+docker-clean: docker-clean-invalid-common
+docker-clean: docker-clean-invalid-golang
+docker-clean: docker-clean-invalid-custom
+docker-clean: docker-clean-invalid-tool
 docker-clean:
+	if [ '${PRUNE}' != '' ] && [ '${PRUNE_AVAILABLE}' == 'true' ]; then docker system prune $(strip $(PRUNE)); fi
+
+.PHONY: docker-clean-invalid-common
+docker-clean-invalid-common:
 	docker images --all \
 	| grep '^<none>\s\+' \
 	| awk '{print $$3}' \
 	| xargs docker rmi -f &>/dev/null || true
 
+.PHONY: docker-clean-invalid-golang
+docker-clean-invalid-golang:
 	docker images --all \
 	| grep '^golang\s\+' \
 	| awk '{print $$2 "\t" $$3}' \
@@ -16,6 +26,8 @@ docker-clean:
 	| awk '{print $$2}' \
 	| xargs docker rmi -f &>/dev/null || true
 
+.PHONY: docker-clean-invalid-custom
+docker-clean-invalid-custom:
 	docker images --all \
 	| grep '^kamilsk\/golang\s\+' \
 	| awk '{print $$2 "\t" $$3}' \
@@ -23,4 +35,11 @@ docker-clean:
 	| awk '{print $$2}' \
 	| xargs docker rmi -f &>/dev/null || true
 
-	if [ '${PRUNE}' != '' ] && [ '${PRUNE_AVAILABLE}' == 'true' ]; then docker system prune $(strip $(PRUNE)); fi
+.PHONY: docker-clean-invalid-tool
+docker-clean-invalid-tool:
+	docker images --all \
+	| grep '^kamilsk\/go-tools\s\+' \
+	| awk '{print $$2 "\t" $$3}' \
+	| grep '^<none>\s\+' \
+	| awk '{print $$2}' \
+	| xargs docker rmi -f &>/dev/null || true
