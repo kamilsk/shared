3b835f8d208c8ed1890c16e2dc8945db5a6d2263
Kamil Samigullin <kamil@samigullin.info>
2017-01-25 15:29:43 +0300

    fix issue #79

--- env.mk
+++ env.mk
@@ -13,3 +13,16 @@ GO_PACKAGE := $(patsubst %/,%,$(subst $(GOPATH)/src/,,$(dir $(abspath $(firstwor
 
 ARGS     =
 PACKAGES = go list ./... | grep -v /vendor/
+define BUILD_VARIATION = (
+    [android]   = [arm],
+    [darwin]    = [386, amd64, arm, arm64],
+    [dragonfly] = [amd64],
+    [freebsd]   = [386, amd64, arm],
+    [linux]     = [386, amd64, arm, arm64, ppc64, ppc64le, mips64, mips64le],
+    [netbsd]    = [386, amd64, arm],
+    [openbsd]   = [386, amd64, arm],
+    [plan9]     = [386, amd64],
+    [solaris]   = [amd64],
+    [windows]   = [386, amd64],
+)
+endef
