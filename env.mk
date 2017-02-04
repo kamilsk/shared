3eb30d014f02b56dbbd4c9684e7a4c708f073309
Kamil Samigullin <kamil@samigullin.info>
2017-02-04 19:19:29 +0300

    fix issue #89

--- env.mk
+++ env.mk
@@ -9,7 +9,7 @@ DATE       := $(shell date -u "+%Y-%m-%d %H:%M:%S")
 GO_VERSION := $(shell go version | awk '{print $$3}' | tr -d 'go')
 
 GIT_REV    := $(shell git rev-parse --short HEAD)
-GO_PACKAGE := $(patsubst %/,%,$(subst $(GOPATH)/src/,,$(dir $(abspath $(firstword $(MAKEFILE_LIST))))))
+GO_PACKAGE := $(patsubst %/,%,$(subst $(GOPATH)/src/,,$(CWD)))
 
 ARGS     =
 PACKAGES = go list ./... | grep -v /vendor/
