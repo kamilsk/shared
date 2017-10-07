e7523bfac34fcd824f7a15b126c69a43f5767a34
Kamil Samigullin <kamil@samigullin.info>
2017-10-07 20:17:01 +0300

    fix issue #250: reduce code duplication

--- docker/base.mk
+++ docker/base.mk
@@ -1,3 +1,10 @@
+COMPLEX_BENCH_ARGS                ?= -benchmem
+COMPLEX_TESTS_ARGS                ?= -timeout=1s
+COMPLEX_TESTS_WITH_COVERAGE_ARGS  ?= $(COMPLEX_TESTS_ARGS)
+PARALLEL_BENCH_ARGS               ?= $(COMPLEX_BENCH_ARGS)
+PARALLEL_TESTS_ARGS               ?= $(COMPLEX_TESTS_ARGS)
+PARALLEL_TESTS_WITH_COVERAGE_ARGS ?= $(COMPLEX_TESTS_WITH_COVERAGE_ARGS)
+
 define docker_base_tpl
 
 .PHONY: docker-pull-$(1)
@@ -86,3 +93,57 @@ endef
 
 render_docker_base_tpl = $(eval $(call docker_base_tpl,$(version)))
 $(foreach version,$(SUPPORTED_VERSIONS),$(render_docker_base_tpl))
+
+.PHONY: complex-bench
+complex-bench: ARGS = $(COMPLEX_BENCH_ARGS)
+define complex_bench_tpl
+complex-bench: docker-bench-$(1)
+endef
+
+render_complex_bench_tpl = $(eval $(call complex_bench_tpl,$(version)))
+$(foreach version,$(SUPPORTED_VERSIONS),$(render_complex_bench_tpl))
+
+.PHONY: complex-tests
+complex-tests: ARGS = $(COMPLEX_TESTS_ARGS)
+define complex_tests_tpl
+complex-tests: docker-test-$(1)
+endef
+
+render_complex_tests_tpl = $(eval $(call complex_tests_tpl,$(version)))
+$(foreach version,$(SUPPORTED_VERSIONS),$(render_complex_tests_tpl))
+
+.PHONY: complex-tests-with-coverage
+complex-tests-with-coverage: ARGS = $(COMPLEX_TESTS_WITH_COVERAGE_ARGS)
+define complex_tests_with_coverage_tpl
+complex-tests-with-coverage: docker-test-with-coverage-$(1)
+endef
+
+render_complex_tests_with_coverage_tpl = $(eval $(call complex_tests_with_coverage_tpl,$(version)))
+$(foreach version,$(SUPPORTED_VERSIONS),$(render_complex_tests_with_coverage_tpl))
+
+.PHONY: parallel-bench
+parallel-bench: ARGS = $(PARALLEL_BENCH_ARGS)
+parallel-bench:
+	semaphore create
+	for v in $(SUPPORTED_VERSIONS); do \
+	    semaphore add -- make docker-bench-$$v ARGS=$(ARGS); \
+	done
+	semaphore wait
+
+.PHONY: parallel-tests
+parallel-tests: ARGS = $(PARALLEL_TESTS_ARGS)
+parallel-tests:
+	semaphore create
+	for v in $(SUPPORTED_VERSIONS); do \
+	    semaphore add -- make docker-test-$$v ARGS=$(ARGS); \
+	done
+	semaphore wait
+
+.PHONY: parallel-tests-with-coverage
+parallel-tests-with-coverage: ARGS = $(PARALLEL_TESTS_WITH_COVERAGE_ARGS)
+parallel-tests-with-coverage:
+	semaphore create
+	for v in $(SUPPORTED_VERSIONS); do \
+	    semaphore add -- docker-test-with-coverage-$$v ARGS=$(ARGS); \
+	done
+	semaphore wait
