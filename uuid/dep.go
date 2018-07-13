481cb73ef9cd0d093890dff5bbfa3eabaf5c9262
Kamil Samigullin <kamil@samigullin.info>
2018-07-13 17:13:06 +0300

    issue #383: add more packages

--- uuid/dep.go
+++ uuid/dep.go
@@ -5,5 +5,9 @@ import (
 	_ "github.com/hashicorp/go-uuid"
 	_ "github.com/jakehl/goid"
 	_ "github.com/kevinburke/go.uuid"
+	_ "github.com/oklog/ulid"
+	_ "github.com/pborman/uuid"
 	_ "github.com/satori/go.uuid"
+	_ "github.com/segmentio/ksuid"
+	_ "gopkg.in/myesui/uuid.v1"
 )
